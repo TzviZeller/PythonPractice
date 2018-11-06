4f2f18bf63f7dfdefbdbc8921cda2c1a967893e6
Tzvi Zeller <tzeller@google.com>
2018-11-06 12:41:29 -0500

    ex1-3

---
+++ PythonTheHardWay/ex1.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+print('Hello Again')
+print('I like typing this')
+print('This is fun')
+print('Yay! Printing')
+
+#The preceding lines are simple but the following is interesting as you can use "" or '' within the other
+print("I would rather 'not' ")
+print('I "said not"')
---
+++ PythonTheHardWay/ex3.py
@@ -0,0 +1,17 @@
+
+print('I will now count my chickens')
+print('hens', 25+30/6)
+print('Rossters', 100-25 *3%4)
+print('Now I will cont the Eggs:')
+print(3+2+1-5+4%2-1/4+6)
+print('Is it true that 3+2<5-7?')
+print(3+2<5-7)
+print('What is 3+2?', 3+2)
+print('What is 5-7?', 5-7)
+
+
+print('oh, thats why its false')
+print('How about more')
+print('Is it greater?', 5>-2)
+print('Is it greater or equal?', 5 >=-2)
+print('Is it less or equal?',5<=-2)
